b94591735f766969b554bd1a8d96f52cccd42afe
fish <fish@DESKTOP-SQQBOOU>
2021-03-13 16:00:44 +0900

    Initial commit

---
+++ src/Cat.java
@@ -0,0 +1,13 @@
+
+public class Cat extends Animal {
+
+	@Override
+	void crying() {
+		
+		System.out.println("고양이 : 야옹~~");
+		
+	}
+	
+	
+
+}
---
+++ src/Dog.java
@@ -0,0 +1,11 @@
+
+public class Dog extends Animal {
+
+	@Override
+	void crying() {
+		
+		System.out.println("강아지 : 월! 월!");
+		
+	}
+
+}
